5d482a0be6422d0350302d9e7a3f2b8762109909
Zichen "Charles" Zhang <zcczhang1@gmail.com>
2024-08-16 11:50:27 -0700

    py>=3.9

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         ]
     },
     install_requires=_read_install_requires(),
-    python_requires="==3.9.*",
+    python_requires=">=3.9.*",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
